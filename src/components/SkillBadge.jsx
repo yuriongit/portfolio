87423d536bb8d9edcbae217891d4bbb90f3fff69
Yuri O. <yuri.dev44@outlook.com>
2026-08-29 10:13:04 -0400

    fix: update icons path

--- src/components/SkillBadge.jsx
+++ src/components/SkillBadge.jsx
@@ -7,7 +7,7 @@ export const SkillBadge = ({ name }) => {
       className="hover:cursor-default inline-flex items-center gap-1.5 rounded-md px-2 py-1 border border-blue-500 bg-blue-500/90 text-sm font-medium text-white"
     >
       {icon && (
-        <img src={`src/public/skill-icons/${icon}.svg`} alt="" className="size-5 saturate-0 invert" loading="lazy" />
+        <img src={`/skill-icons/${icon}.svg`} alt="" className="size-5 saturate-0 invert" loading="lazy" />
       )}
       {name}
     </span>
