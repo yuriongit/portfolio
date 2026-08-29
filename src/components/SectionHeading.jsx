638a1955060f3949c2085ae712223991e935460d
Yuri O. <yuri.dev44@outlook.com>
2026-08-29 10:08:18 -0400

    refactor: remove remainder dark classes

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -1,6 +1,6 @@
 export const SectionHeading = ({ children }) => {
   return (
-    <h2 className="mb-7 flex items-center text-xl font-extrabold text-neutral-900 dark:text-neutral-100 underline decoration-dashed underline-offset-6 decoration-blue-500 dark:decoration-blue-500">
+    <h2 className="mb-7 flex items-center text-2xl font-extrabold text-neutral-900 underline decoration-dashed underline-offset-6 decoration-blue-500">
       <span className="" />
       {children}
     </h2>
